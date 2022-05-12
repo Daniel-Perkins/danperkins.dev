04432afdb9359c1208a6b016d06ca70b6263e05a
Daniel-Perkins <danjonperkins@gmail.com>
2022-05-12 16:04:18 +0100

    About responsive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,17 @@
 const About = () => {
   return (
     <div className="flex flex-col items-center m-16">
-      <div className="flex flex-col items-center m-16">
-        <h1 className="text-3xl">Websites For Local Businesses</h1>
-        <h3>Tailored to reduce hassle with budgets in mind</h3>
+      <div className="flex flex-col items-center my-16 md:m-16">
+        <h1 className="text-xl text-center sm:text-3xl">
+          Websites For Local Businesses
+        </h1>
+        <h3 className="text-sm font-light text-center sm:font-normal sm:text-base">
+          Tailored to reduce hassle with budgets in mind
+        </h3>
       </div>
-      <div className="grid grid-cols-3 gap-20 font-light">
+      <div className="grid gap-8 font-light sm:gap-20 sm:grid-cols-2 md:grid-cols-3">
         <div>
-          <h4 className="mb-3 text-xl">Time</h4>
+          <h4 className="text-xl sm:mb-3">Time</h4>
           <p>
             Static sites made in a couple of days to get your business online
             and ready as soon as possible. Regularly communicating with the
@@ -15,14 +19,14 @@ const About = () => {
           </p>
         </div>
         <div>
-          <h4 className="mb-3 text-xl">Tools</h4>
+          <h4 className="text-xl sm:mb-3">Tools</h4>
           <p>
             Coded websites are far more customisable than website builders,
             allowing me to tailor your site best for your business’ needs
           </p>
         </div>
         <div>
-          <h4 className="mb-3 text-xl">Deployment</h4>
+          <h4 className="text-xl sm:mb-3">Deployment</h4>
           <p>
             I host websites with Netlify which allows for free hosting with the
             scale of a small business. To find out about netlify{" "}
@@ -35,7 +39,7 @@ const About = () => {
           </p>
         </div>
         <div>
-          <h4 className="mb-3 text-xl">Setup</h4>
+          <h4 className="text-xl sm:mb-3">Setup</h4>
           <p>
             I can walk you through website setup or I can manage all of the
             aspects of your website at a very affordable price and keep the
@@ -43,14 +47,14 @@ const About = () => {
           </p>
         </div>
         <div>
-          <h4 className="mb-3 text-xl">Domains</h4>
+          <h4 className="text-xl sm:mb-3">Domains</h4>
           <p>
             I can help you find the best domain possible at the best price. A
             good domain greatly increases discoverability for your business.
           </p>
         </div>
         <div>
-          <h4 className="mb-3 text-xl">Tailwind CSS</h4>
+          <h4 className="text-xl sm:mb-3">Tailwind CSS</h4>
           <p>
             I use a tool that allows for website creation on the fly, far faster
             than traditional CSS styling with zero compromises
